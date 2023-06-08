ef5c5795e7ef5ab1a94076abe45129c5ace23159
Mhijazi16 <gultimate0@gmail.com>
2023-06-08 21:49:32 +0300

    create AddEnemyToEpisode() method and AddCompanionToEpisode() method

--- DoctorWho/DoctorWhoRepository/EpisodeRepository.cs
+++ DoctorWho/DoctorWhoRepository/EpisodeRepository.cs
@@ -44,17 +44,46 @@ public class EpisodeRepository
            _context.SaveChanges(); 
        }
 
-       public static void setEpisodeAuthor(Episode episode, int authorId)
+       public static void SetEpisodeAuthor(Episode episode, int authorId)
        {
            episode.AuthorId = authorId;
            _context.Episodes.Update(episode);
            _context.SaveChanges(); 
        }
 
-       public static void setEpisodeDoctor(Episode episode, int doctorId)
+       public static void SetEpisodeDoctor(Episode episode, int doctorId)
        {
            episode.DoctorId = doctorId;
            _context.Episodes.Update(episode);
            _context.SaveChanges();
        }
+
+       public static void AddEnemyToEpisode(Episode episode, Enemy enemy)
+       {
+           int epID = episode.EpisodeId;
+           int enID = enemy.EnemyId;
+           
+           var record = new EpisodeEnemy{ EpisodeId = epID, EnemyId = enID, Enemy = enemy, Episode = episode};
+           episode.TblEpisodeEnemies.Add(record);
+           enemy.TblEpisodeEnemies.Add(record);
+
+           _context.Update(enemy);
+           _context.Update(episode);
+           _context.SaveChanges();
+       }
+
+       public static void AddCompanionToEpisode(Episode episode, Companion companion)
+       {
+           int epID = episode.EpisodeId;
+           int cpID = companion.CompanionId;
+
+           var record = new EpisodeCompanion
+               { EpisodeId = epID, CompanionId = cpID, Episode = episode, Companion = companion }; 
+           episode.TblEpisodeCompanions.Add(record);
+           companion.TblEpisodeCompanions.Add(record);
+
+           _context.Update(episode);
+           _context.Update(companion); 
+       }
+       
 }
