9afa6ecb52a767b54dd89fc535550d25db2376ab
Mhijazi16 <gultimate0@gmail.com>
2023-06-08 14:57:53 +0300

    Repository : create AddAuthor() methods to add Authors to Database.

--- DoctorWho/DoctorWhoRepository/AuthorRepo.cs
+++ DoctorWho/DoctorWhoRepository/AuthorRepo.cs
@@ -4,5 +4,17 @@ namespace DoctorWhoRepository;
 
 public static class AuthorRepo
 {
+    public static void AddAuthor(Author author)
+    {
+        var context = new DoctorWhoContext();
+        context.Authors.Add(author);
+        context.SaveChanges(); 
+    }
 
+    public static void AddAuthor(int id, string Name)
+    {
+        var context = new DoctorWhoContext();
+        context.Authors.Add(new Author{AuthorId = id,AuthorName = Name});
+        context.SaveChanges(); 
+    }
 }
