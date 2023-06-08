e50440d7ffaafc496b4ed56bfb8724d6445a47a9
Mhijazi16 <gultimate0@gmail.com>
2023-06-08 15:27:08 +0300

    Repository : refactor code.

--- DoctorWho/DoctorWhoRepository/AuthorRepo.cs
+++ DoctorWho/DoctorWhoRepository/AuthorRepo.cs
@@ -6,15 +6,19 @@ public static class AuthorRepo
 {
     public static void AddAuthor(Author author)
     {
-        var context = new DoctorWhoContext();
-        context.Authors.Add(author);
-        context.SaveChanges(); 
+        using (var context = new DoctorWhoContext())
+        {
+            context.Authors.Add(author); 
+            context.SaveChanges(); 
+        }
     }
 
     public static void AddAuthor(int id, string Name)
     {
-        var context = new DoctorWhoContext();
-        context.Authors.Add(new Author{AuthorId = id,AuthorName = Name});
-        context.SaveChanges(); 
+        using (var context = new DoctorWhoContext())
+        {
+            context.Authors.Add(new Author{AuthorId = id,AuthorName = Name});
+            context.SaveChanges(); 
+        }
     }
 }
